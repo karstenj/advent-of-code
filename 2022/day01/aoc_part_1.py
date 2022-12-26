bdf88f967a64b3df2c2614f428b2f9813075d0be
Karsten <kjuschus@freenet.de>
2022-12-26 14:05:52 +0100

    Solution Day1

--- 2022/day01/aoc_part_1.py
+++ 2022/day01/aoc_part_1.py
@@ -1,17 +1,16 @@
 def get_number_part1(input_file_name):
-    result = 0
     with open(input_file_name) as fh:
         lines = fh.readlines()
+    v = []
     s = 0
-    max = []
     for l in lines:
         l = l.strip()
-        if l == '':
-            max.append(s)
-            s = 0
-        else:
+        if l != '':
             s += int(l)
-    max.append(s)
-    result = sorted(max)
-    print(result[-1])
-    return result[-1]
+        else:
+            v.append(s)
+            s = 0
+    v.append(s)
+    v = sorted(v)
+    #print(v)
+    return v[-1]
