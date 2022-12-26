544326d68cdb41199990b5f75bc98f082fe08304
Karsten <kjuschus@freenet.de>
2022-12-26 14:42:25 +0100

    Solution Day12

---
+++ 2022/day12/aoc_part_2.py
@@ -0,0 +1,45 @@
+
+def find_path(nodes, costs, grid):
+    node = nodes.pop()
+    x, y = node
+    c = costs[f'{x}:{y}']
+    next_nodes = []
+    if x > 0:
+        next_nodes.append((x-1, y))
+    if x < len(grid)-1:
+        next_nodes.append((x + 1, y))
+    if y < len(grid[x])-1:
+        next_nodes.append((x, y+1))
+    if y > 0:
+        next_nodes.append((x, y-1))
+    prev = grid[x][y]
+    for xn, yn in next_nodes:
+        act = grid[xn][yn]
+        if (act == 'E' and prev == 'z') or (act == 'a' and prev == 'S') or (act not in ['S', 'E'] and ord(act)-ord(prev) <= 1):
+            # valid edge
+            key = f'{xn}:{yn}'
+            if not key in costs or costs[key] > (c + 1):
+                #print(f"{xn} {yn} {prev} {act} {c+1}")
+                nodes.add((xn, yn))
+                costs[key] = c + 1
+
+
+def get_number_part2(input_file_name):
+    with open(input_file_name) as fh:
+        lines = fh.readlines()
+    grid = []
+    nodes = set()
+    costs = {}
+    for i, l in enumerate(lines):
+        l = l.strip()
+        if 'a' in l:
+            x, y = i, l.find('a')
+            nodes.add((x, y))
+            costs[f'{x}:{y}'] = 0
+        if 'E' in l:
+            xe, ye = i, l.find('E')
+        grid.append(l)
+    while len(nodes) > 0:
+        find_path(nodes, costs, grid)
+    #print(costs[f'{xe}:{ye}'])
+    return costs[f'{xe}:{ye}']
